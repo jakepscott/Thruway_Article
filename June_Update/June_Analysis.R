3d5a0547cc746266609f09f54de06762ca846ab7
jakepscott <jakepscott16@gmail.com>
2020-07-08 11:44:30 -0400

    Updated June update to include data up to July 7th. Also added a holiday traffic figure

--- June_Update/June_Analysis.R
+++ June_Update/June_Analysis.R
@@ -12,6 +12,7 @@ library(magick)
 library(maps)
 library(zoo)
 library(patchwork)
+library(ggrepel)
 windowsFonts(`Roboto Condensed` = windowsFont("Roboto Condensed"))
 
 
@@ -33,7 +34,7 @@ NYS_Thruway_Data_lockdown <- NYS_Thruway_Data %>%
 
 traffic2019vs2020 <- 
   NYS_Thruway_Data %>%
-  filter((Date<="2020-07-02" & Date>="2020-01-01" | Date<="2020-07-02" & Date>="2019-01-01")) %>% 
+  filter((Date<="2020-07-07" & Date>="2020-01-01" | Date<="2020-07-07" & Date>="2019-01-01")) %>% 
   group_by(Date) %>% 
   summarise(Total=sum(Vehicle.Count)) %>%
   ungroup() %>% 
@@ -63,7 +64,7 @@ traffic2019vs2020 <-
   geom_vline(xintercept = as.numeric(traffic2019vs2020$Date.y[77]), 
              color="grey50", lwd=1, linetype="dashed") +
   scale_color_manual(values = c("#C29000","#0A8CC2")) +
-  labs(title = "Traffic had been rising since late April, but has plateaued \nas cases have risen across the country",
+  labs(title = "Traffic had plateaued as cases rose across the country,\nbut appears to be back on its upward trajectory",
        subtitle = "Vertical lines represent national and statewide lockdowns, respectively",
        x=NULL,
        y= "Percent of 2019 Traffic", 
@@ -180,7 +181,7 @@ ggplot() +
         plot.title.position = "plot",
         legend.key = element_rect(fill = "white", colour = "white"))
 
-#ggsave("June_Update/HeatMap.png", dpi=600)
+ggsave("June_Update/HeatMap.png", dpi=600)
 
 
 # Location Map ------------------------------------------------------------
@@ -325,7 +326,7 @@ b_mgif <- image_read(gif_2020)
 ##Comparing to week last year
 com_verus_noncom_total_index <- 
   NYS_Thruway_Data %>%
-  filter((Date<="2020-07-02" & Date>="2020-01-01" | Date<="2020-07-02" & Date>="2019-01-01")) %>% 
+  filter((Date<="2020-07-07" & Date>="2020-01-01" | Date<="2019-07-07" & Date>="2019-01-01")) %>% 
   group_by(year, week, commercial_vehicle) %>% 
   summarise(Total=sum(Vehicle.Count)) %>%
   ungroup() 
@@ -338,8 +339,7 @@ com_verus_noncom_total_index <-
 
 com_verus_noncom_total_index <- 
   com_verus_noncom_total_index %>% 
-  mutate(index=(Total.y/Total.x)*100) %>% 
-  filter(week<=26)
+  mutate(index=(Total.y/Total.x)*100) 
 
 
 ggplot(com_verus_noncom_total_index) +
@@ -370,7 +370,7 @@ ggplot(com_verus_noncom_total_index) +
         plot.title.position = "plot", 
         axis.title.y = element_text(vjust = .85)) 
 
-#ggsave("June_Update/Com_V_NonCom.png", dpi=600)
+ggsave("June_Update/Com_V_NonCom.png", dpi=600)
 
 
 
@@ -379,7 +379,7 @@ ggplot(com_verus_noncom_total_index) +
 
 com_verus_noncom_total_index_by_exit <- 
   NYS_Thruway_Data %>%
-  filter((Date<="2020-07-02" & Date>="2020-01-01" | Date<="2019-07-02" & Date>="2019-01-01")) %>% 
+  filter((Date<="2020-07-07" & Date>="2020-01-01" | Date<="2019-07-07" & Date>="2019-01-01")) %>% 
   group_by(Exit, year, week, commercial_vehicle) %>% 
   summarise(Total=sum(Vehicle.Count)) %>%
   ungroup() 
@@ -407,7 +407,7 @@ ggplot(com_verus_noncom_total_index_by_exit) +
   facet_wrap(~Exit)+
   scale_color_manual(values = c("#C29000","#0A8CC2")) +
   scale_y_continuous(breaks = NULL) +
-  labs(title = "Across the state, traffic of all types is starting to level off",
+  labs(title = "There is heterogeneity in the recovery across exits",
        x="Week",
        y= "Percent of 2019 Traffic", 
        color="Vehicle Type",
@@ -427,4 +427,55 @@ ggplot(com_verus_noncom_total_index_by_exit) +
         axis.title.x = element_blank()) +
   guides(color=guide_legend(title.position = "left"))
 
-#ggsave("June_Update/Commercial_vs_nonCommercial_by_Exit.png", dpi=600)
+ggsave("June_Update/Commercial_vs_nonCommercial_by_Exit.png", dpi=600)
+
+
+
+
+
+# July 4th and Memorial Day and St. Patricks and new years -----------------------------------------------
+
+traffic2019vs2020 %>% filter(day(Date.x)=="4" & month(Date.x)=="7")
+traffic2019vs2020 %>% filter(day(Date.x)=="27" & month(Date.x)=="5")
+traffic2019vs2020 %>% filter(day(Date.x)=="25" & month(Date.x)=="5")
+traffic2019vs2020 %>% filter(day(Date.x)=="17" & month(Date.x)=="3")
+traffic2019vs2020 %>% filter(day(Date.x)=="1" & month(Date.x)=="1")
+
+#New YEars
+256146/268075*100
+#St. Patrick's Day 2019 vs 2020
+306801/336926*100
+#Memorial day 2019 2020
+215990/382485*100
+#Fourth 2019 vs 2020
+254736/363191*100
+
+holidays <- tibble(holiday=c("New Years Day","St. Patrick's Day","Memorial Day","4th of July"),
+       percent=c(95.55013,91.05887,56.47019,70.1383))
+
+holidays$holiday <- factor(holidays$holiday,
+          labels=c("New Years Day","St. Patrick's Day","Memorial Day","4th of July"),
+          levels=c("New Years Day","St. Patrick's Day","Memorial Day","4th of July"))
+
+ggplot(holidays,aes(x=(holiday),y=percent, group=1)) + 
+  geom_point(color="#0A8CC2",size=5) +
+  coord_cartesian(ylim=c(50,100)) +
+  geom_text_repel(aes(label=holiday),direction = "y",box.padding = .75,point.padding = 1,segment.color = "grey70") +
+  geom_line(color="#0A8CC2",lwd=2) +
+  labs(title = "Memorial Day was the nadir of holiday traffic, but even\ntraffic on the 4th was well below last year's levels",
+       x=NULL,
+       y= "Percent of 2019 Traffic", 
+       caption = "Plot: @jakepscott2020 | Data: data.ny.gov/Transportation") +
+  theme_minimal(base_family = "Roboto Condensed", base_size = 12) +
+  theme(panel.grid = element_blank(),
+        plot.title = element_text(face = "bold", size = rel(1.5)),
+        plot.subtitle = element_text(face = "plain", size = rel(1.1), color = "grey70"),
+        plot.caption = element_text(face = "italic", size = rel(0.8), 
+                                    color = "grey70"),
+        legend.title = element_text(size = rel(1.1), face="bold"),
+        legend.text = element_text(size=rel(.9)),
+        legend.position = "bottom",
+        plot.title.position = "plot", 
+        axis.title.y = element_text(vjust = .85),
+        axis.text.x=element_blank()) 
+ggsave("June_Update/Holidays.png", dpi=600)
