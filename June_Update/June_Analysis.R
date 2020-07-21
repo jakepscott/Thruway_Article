011dab1b47b0b0880f6eadecbeff4b735251a8a3
jakepscott <jakepscott16@gmail.com>
2020-07-21 15:09:45 -0400

    Simply commented out ggsave so as to not overwrite previous figures accidentally

--- June_Update/June_Analysis.R
+++ June_Update/June_Analysis.R
@@ -81,60 +81,7 @@ traffic2019vs2020 <-
         legend.position = "bottom",
         plot.title.position = "plot", 
         axis.title.y = element_text(vjust = .85)))
-ggsave("June_Update/Overall2019vs2020.png", dpi=600)
-
-
-
-# Vehicles by Weekday ---------------------------------------------
-
-vehicles_by_weekday_2019vs2020 <- 
-  NYS_Thruway_Data_lockdown %>% 
-  group_by(lockdownperiod,Entrance,Date) %>% 
-  mutate(Total_By_Date=sum(Vehicle.Count)) %>% ungroup() %>%
-  group_by(lockdownperiod,year, weekday) %>% 
-  summarise(Mean_Vehicles=mean(Total_By_Date))
-
-vehicles_by_weekday_2019vs2020 <- 
-  vehicles_by_weekday_2019vs2020 %>% 
-  mutate(year_label=case_when(lockdownperiod=="Pre-Lockdown Period" &
-                                weekday=="Monday" & year==2019~"2019",
-                              lockdownperiod=="Pre-Lockdown Period" &
-                                weekday=="Monday" & year==2020~"2020",
-                              lockdownperiod=="Post-Lockdown Period" &
-                                weekday=="Monday" & year==2019~"2019",
-                              lockdownperiod=="Post-Lockdown Period" &
-                                weekday=="Monday" & year==2020~"2020")) 
-
-
-ggplot(vehicles_by_weekday_2019vs2020) +
-  geom_col(aes(x=weekday,y=Mean_Vehicles, fill=year),
-           position = position_dodge()) +
-  geom_text(aes(x=weekday,y=Mean_Vehicles, color=year, label=year_label),
-            position = position_dodge(width = 0.9),vjust=-.2,
-            family="Roboto Condensed") +
-  facet_wrap(~fct_rev(factor(lockdownperiod)), ncol = 1) +
-  scale_color_manual(values = c("grey70", "#C29000")) +
-  scale_fill_manual(values = c("grey70", "#C29000")) +
-  labs(title = "Mean Number of Vehicles Passing Entry Points by Day of Week",
-       subtitle = "There is a clear fall in the number of vehicles entering toll booths following lockdown",
-       y="Mean Number\nof Vehicles",
-       x=NULL,
-       fill=NULL,
-       caption = "Plot: @jakepscott2020 | Data: data.ny.gov/Transportation") +
-  theme_minimal(base_family = "Roboto Condensed", base_size = 12) +
-  theme(panel.grid = element_blank(),
-        plot.title = element_text(face = "bold", size = rel(1.2)),
-        plot.subtitle = element_text(face = "plain", size = rel(.9), color = "grey70"),
-        plot.caption = element_text(size = rel(0.9), face="italic", 
-                                    color = "grey70"),
-        strip.text = element_text(face="bold",size = rel(1)),
-        legend.title = element_text(size = rel(.8)),
-        plot.title.position = "plot", 
-        axis.title.y = element_blank(),
-        legend.position = "none") +
-  coord_cartesian(ylim=c(0,15000))
-
-#ggsave("July_Update//Vehicles_by_Weekday.png", dpi=600)
+#ggsave("June_Update/Overall2019vs2020.png", dpi=600)
 
 
 
@@ -181,143 +128,7 @@ ggplot() +
         plot.title.position = "plot",
         legend.key = element_rect(fill = "white", colour = "white"))
 
-ggsave("June_Update/HeatMap.png", dpi=600)
-
-
-# Location Map ------------------------------------------------------------
-
-
-#Getting the number of vehicles by exit for each date
-Entrance_Data_2020 <- NYS_Thruway_Data %>% 
-  group_by(Date, Entrance) %>%
-  summarise(total=sum(Vehicle.Count)) %>% 
-  ungroup() %>%
-  rename(Exit=Entrance) %>%
-  mutate(Date=ymd(Date),
-         year=year(Date),
-         month=month(Date),
-         day=day(Date),
-         weekday=weekdays(as.Date(Date))) %>%
-  filter(year==2020 & month>=3)
-
-#Getting the latitude and longitude for the exits
-Locations <- locations %>% select(Exit, Latitude, Longitude)
-
-#Combining the number of vehicles and the lat and long
-Entrance_Data_2020 <- left_join(Entrance_Data_2020,Locations,by="Exit")
-
-#Standardizing number of cars
-Entrance_Data_2020 <- 
-  Entrance_Data_2020 %>% 
-  group_by(Exit,weekday) %>% 
-  mutate(mean=mean(total), sd=sd(total)) %>%
-  ungroup() %>% 
-  mutate(z_score=(total-mean)/sd)
-
-#2020 Gif using gganimate
-gif_2020 <- ggplot() + 
-  geom_polygon(data=NY, aes(x=long, y=lat, group=group),
-               color="black", fill="lightblue") +
-  geom_point(data=Entrance_Data_2020, aes(x=Longitude, y=Latitude, size=z_score,
-                                          color=z_score)) +
-  scale_color_viridis_c(option = "inferno", labels = comma) + 
-  scale_size(range = c(.1, 10)) +
-  labs(title="Number of Vehicles at a Given Exit on {frame_time}",
-       subtitle = "Traffic in 2020 mirrors that of 2019... until the stay-at-home order was announced on March 21st",
-       x="Longitude",
-       y="Latitude",
-       color= "Z Score",
-       size="Z Score",
-       caption = "Plot: @jakepscott2020 | Data: data.ny.gov/Transportation") +
-  theme_minimal(base_family = "Roboto Condensed", base_size = 12) +
-  theme(panel.grid = element_blank(),
-        plot.title = element_text(face = "bold", size = rel(1.2)),
-        plot.subtitle = element_text(face = "plain", size = rel(.9), color = "grey70"),
-        plot.caption = element_text(face = "italic", size = rel(0.9), 
-                                    color = "grey70"),
-        legend.title = element_text(size = rel(.8)),
-        plot.title.position = "plot", 
-        axis.title.y = element_text(angle=0, vjust = .5)) +
-  transition_time(Date)
-
-#Animating the gif
-gif_2020 <- animate(gif_2020, renderer = gifski_renderer(), 
-                    nframes=length(unique(Entrance_Data_2020$Date)),
-                    end_pause = 5,
-                    width = 500, 
-                    height = 500)
-
-#Getting the number of vehicles by exit for each date for 2019
-Entrance_Data_2019 <- NYS_Thruway_Data %>% 
-  group_by(Date, Entrance) %>%
-  summarise(total=sum(Vehicle.Count)) %>% 
-  ungroup() %>%
-  rename(Exit=Entrance) %>%
-  mutate(Date=ymd(Date),
-         year=year(Date),
-         month=month(Date),
-         day=day(Date),
-         weekday=weekdays(as.Date(Date))) %>% 
-  filter(year==2019 & month>=3 & Date<="2019-06-29")
-
-
-#Combining the number of vehicles and the lat and long
-Entrance_Data_2019 <- left_join(Entrance_Data_2019,Locations,by="Exit")
-
-#Standardizing number of cars
-Entrance_Data_2019 <- 
-  Entrance_Data_2019 %>% 
-  group_by(Exit,weekday) %>% 
-  mutate(mean=mean(total), sd=sd(total)) %>%
-  ungroup() %>% 
-  mutate(z_score=(total-mean)/sd)
-
-##Making gif with gganimate
-gif_2019 <- ggplot() + 
-  geom_polygon(data=NY, aes(x=long, y=lat, group=group),
-               color="black", fill="lightblue") +
-  geom_point(data=Entrance_Data_2019, aes(x=Longitude, y=Latitude, size=z_score,
-                                          color=z_score)) +
-  scale_color_viridis_c(option = "inferno", labels = comma) + 
-  scale_size(range = c(.1, 10)) +
-  labs(title="Number of Vehicles at a Given Exit on {frame_time}",
-       subtitle = "Besides some drops (likely due to weather), traffic in 2019 is fairly consistent",
-       x="Longitude",
-       y="Latitude",
-       color= "Z Score",
-       size="Z Score") +
-  theme_minimal(base_family = "Roboto Condensed", base_size = 12) +
-  theme(panel.grid = element_blank(),
-        plot.title = element_text(face = "bold", size = rel(1.2)),
-        plot.subtitle = element_text(face = "plain", size = rel(.9), color = "grey70"),
-        plot.caption = element_text(face = "italic", size = rel(0.3), 
-                                    color = "grey70", hjust = 0),
-        legend.title = element_text(size = rel(.8)),
-        plot.title.position = "plot", 
-        axis.title.y = element_text(angle=0, vjust = .5)) +
-  transition_time(Date)
-
-#animating
-gif_2019 <- animate(gif_2019, renderer = gifski_renderer(), 
-                    nframes=length(unique(Entrance_Data_2019$Date)),
-                    end_pause = 5,
-                    width = 500, 
-                    height = 500)
-#turning the gifs into a series of images using the magik package
-a_mgif <- image_read(gif_2019)
-b_mgif <- image_read(gif_2020)
-
-#Making a new gif with the two next to each other, again using magik
-  new_gif <- image_append(c(a_mgif[1], b_mgif[1]))
-  for(i in 1:length(unique(Entrance_Data_2019$Date))){
-    combined <- image_append(c(a_mgif[i], b_mgif[i]))
-    new_gif <- c(new_gif, combined)
-  }
-  
-  #Saving. For some reason I Cannot get it to save in the Figures Folder
-  #image_write(new_gif, "NYS_Locations.gif", quality = 100)
-  image_write(new_gif, "NYS_Locations_630.gif", quality = 100)
-
+#ggsave("June_Update/HeatMap.png", dpi=600)
 
 
 # Overall Commercial Versus non Commercial ------------------------
@@ -370,7 +181,7 @@ ggplot(com_verus_noncom_total_index) +
         plot.title.position = "plot", 
         axis.title.y = element_text(vjust = .85)) 
 
-ggsave("June_Update/Com_V_NonCom.png", dpi=600)
+#ggsave("June_Update/Com_V_NonCom.png", dpi=600)
 
 
 
@@ -427,7 +238,7 @@ ggplot(com_verus_noncom_total_index_by_exit) +
         axis.title.x = element_blank()) +
   guides(color=guide_legend(title.position = "left"))
 
-ggsave("June_Update/Commercial_vs_nonCommercial_by_Exit.png", dpi=600)
+#ggsave("June_Update/Commercial_vs_nonCommercial_by_Exit.png", dpi=600)
 
 
 
